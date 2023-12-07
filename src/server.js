6ee7e0cdf6b046e4a2f0d2e57c383b4010c3ef89
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-12-07 13:29:36 +1100

    feat: added role router onto server.js

--- src/server.js
+++ src/server.js
@@ -112,8 +112,8 @@ app.get('/', (request, response) => {
     });
 });
 
-
-
+const rolesController = require("./controllers/RoleRoutes");
+app.use("/roles", rolesController);
 
 // Keep this route at the end of this file, only before the module.exports!
 // A 404 route should only trigger if no preceding routes or middleware was run. 
