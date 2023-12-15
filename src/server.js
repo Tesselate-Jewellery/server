6ed03246ed574125096614771030094e8ed7e755
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-12-15 17:12:26 +1100

    feat: edit server.js HOST and PORT default

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ dotenv.config();
 const express = require('express');
 const app = express();
 // If no process.env.X is found, assign a default value instead.
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || 5000;
 
 // Configure some basic Helmet settings on the server instance.
 const helmet = require('helmet');
