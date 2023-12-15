6b1c15dfabee7d0b3a5fbcf5fe10f0e558c53b19
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-12-15 16:35:23 +1100

    feat: removed port variable from index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 // Import the configured items from the server file:
 var {app, PORT, HOST} = require('./server');
 
-// For Heroku deployment
-// Allow the use of environment variable 'PORT' if available (as set by Heroku) or default to 3000
-const PORT = process.env.PORT || 3000;
-
-
 // Run the server
 app.listen(PORT, HOST, () => {
     console.log(`
